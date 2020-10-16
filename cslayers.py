b9842e09ab64380ee19ef145f89c6855669d3ab1
chenzhengrong <30426320+njustczr@users.noreply.github.com>
2020-10-16 18:17:51 +0800

    change cspdarknet53's structure

--- cslayers.py
+++ cslayers.py
@@ -27,6 +27,7 @@ class Conv2dBatchLeaky(nn.Module):
         else:
             self.padding = int(kernel_size/2)
         self.leaky_slope = leaky_slope
+        # self.mish = Mish()
 
         # Layer
         if activation == "leaky":
@@ -65,16 +66,17 @@ class SmallBlock(nn.Module):
         # conv_shortcut
         '''
         参考 https://github.com/bubbliiiing/yolov4-pytorch
-        残差边后面还是接一个1x1conv+bn+mish------20201014
+        shortcut后不接任何conv
         '''
         # self.active_linear = Conv2dBatchLeaky(nchannels, nchannels, 1, 1, activation='linear')
-        self.conv_shortcut = Conv2dBatchLeaky(nchannels, nchannels, 1, 1, activation='mish')
+        # self.conv_shortcut = Conv2dBatchLeaky(nchannels, nchannels, 1, 1, activation='mish')
+
 
     def forward(self, data):
         short_cut = data + self.features(data)
-        active_linear = self.conv_shortcut(short_cut)
+        # active_linear = self.conv_shortcut(short_cut)
 
-        return active_linear
+        return short_cut
 
 # Stage1  conv [256,256,3]->[256,256,32]
 
@@ -84,60 +86,56 @@ class Stage2(nn.Module):
         super().__init__()
         # stage2 32
         self.conv1 = Conv2dBatchLeaky(nchannels, 2*nchannels, 3, 2, activation='mish')
-        self.conv2 = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
-        self.conv3 = Conv2dBatchLeaky(2*nchannels, nchannels, 1, 1, activation='mish')
-        self.conv4 = Conv2dBatchLeaky(nchannels, 2*nchannels, 3, 1, activation='mish')
-        '''
-        参考 https://github.com/bubbliiiing/yolov4-pytorch
-        残差边后面还是接一个1x1conv+bn+mish------20201014
-        '''
-        # self.active_linear = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='linear')
-        self.conv_shortcut = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
-        self.conv5_l = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
-        self.conv5_r = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
+        self.split0 = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
+        self.split1 = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
+
+        self.conv2 = Conv2dBatchLeaky(2*nchannels, nchannels, 1, 1, activation='mish')
+        self.conv3 = Conv2dBatchLeaky(nchannels, 2*nchannels, 3, 1, activation='mish')
+
+        self.conv4 = Conv2dBatchLeaky(2*nchannels, 2*nchannels, 1, 1, activation='mish')
 
 
     def forward(self, data):
         conv1 = self.conv1(data)
-        # route1 = conv1
-        conv2 = self.conv2(conv1)
-        #conv2_bk = conv2
+        split0 = self.split0(conv1)
+        split1 = self.split1(conv1)
+        conv2 = self.conv2(split1)
         conv3 = self.conv3(conv2)
-        conv4 = self.conv4(conv3)
-        shortcut = conv2 + conv4
-        #active_linear = self.active_linear(shortcut)
-        conv_shortcut = self.conv_shortcut(shortcut)
-        conv5_r = self.conv5_r(conv_shortcut)
-        conv5_l = self.conv5_l(conv1)
-        route = torch.cat([conv5_l, conv5_r], dim=1)
 
+        shortcut = split1 + conv3
+        conv4 = self.conv4(shortcut)
+
+        route = torch.cat([split0, conv4], dim=1)
         return route
 
 class Stage3(nn.Module):
-
     def __init__(self, nchannels):
         super().__init__()
         # stage3 128
         self.conv1 = Conv2dBatchLeaky(nchannels, int(nchannels/2), 1, 1, activation='mish')
         self.conv2 = Conv2dBatchLeaky(int(nchannels/2), nchannels, 3, 2, activation='mish')
 
-        self.conv3 = Conv2dBatchLeaky(nchannels, int(nchannels/2), 1, 1, activation='mish')
+        self.split0 = Conv2dBatchLeaky(nchannels, int(nchannels/2), 1, 1, activation='mish')
+        self.split1 = Conv2dBatchLeaky(nchannels, int(nchannels/2), 1, 1, activation='mish')
+
         self.block1 = SmallBlock(int(nchannels/2))
         self.block2 = SmallBlock(int(nchannels/2))
 
-        self.conv4_l = Conv2dBatchLeaky(nchannels, int(nchannels/2), 1, 1, activation='mish')
-        self.conv4_r = Conv2dBatchLeaky(int(nchannels/2), int(nchannels/2), 1, 1, activation='mish')
+        self.conv3 = Conv2dBatchLeaky(int(nchannels/2), int(nchannels/2), 1, 1, activation='mish')
 
     def forward(self, data):
         conv1 = self.conv1(data)
         conv2 = self.conv2(conv1)
-        #route1 = conv2
-        conv3 = self.conv3(conv2)
-        block1 = self.block1(conv3)
+
+        split0 = self.split0(conv2)
+        split1 = self.split1(conv2)
+
+        block1 = self.block1(split1)
         block2 = self.block2(block1)
-        conv4_r = self.conv4_r(block2)
-        conv4_l = self.conv4_l(conv2)
-        route = torch.cat([conv4_l, conv4_r], dim=1)
+
+        conv3 = self.conv3(block2)
+
+        route = torch.cat([split0, conv3], dim=1)
 
         return route
 
@@ -150,23 +148,23 @@ class Stage(nn.Module):
         # stage6 : 512
         self.conv1 = Conv2dBatchLeaky(nchannels, nchannels, 1, 1, activation='mish')
         self.conv2 = Conv2dBatchLeaky(nchannels, 2*nchannels, 3, 2, activation='mish')
-        self.conv3 = Conv2dBatchLeaky(2*nchannels, nchannels, 1, 1, activation='mish')
+        self.split0 = Conv2dBatchLeaky(2*nchannels, nchannels, 1, 1, activation='mish')
+        self.split1 = Conv2dBatchLeaky(2*nchannels, nchannels, 1, 1, activation='mish')
         blocks = []
         for i in range(nblocks):
             blocks.append(SmallBlock(nchannels))
         self.blocks = nn.Sequential(*blocks)
-        self.conv4_l = Conv2dBatchLeaky(2*nchannels, nchannels, 1, 1, activation='mish')
-        self.conv4_r = Conv2dBatchLeaky(nchannels, nchannels, 1, 1, activation='mish')
+        self.conv4 = Conv2dBatchLeaky(nchannels, nchannels, 1, 1, activation='mish')
 
     def forward(self,data):
         conv1 = self.conv1(data)
         conv2 = self.conv2(conv1)
-        #route1 = conv2
-        conv3 = self.conv3(conv2)
-        blocks = self.blocks(conv3)
-        conv4_r = self.conv4_r(blocks)
-        conv4_l = self.conv4_l(conv2)
-        route = torch.cat([conv4_l, conv4_r], dim=1)
+
+        split0 = self.split0(conv2)
+        split1 = self.split1(conv2)
+        blocks = self.blocks(split1)
+        conv4 = self.conv4(blocks)
+        route = torch.cat([split0, conv4], dim=1)
 
         return route
 
